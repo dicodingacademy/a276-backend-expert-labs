83ae10695fd7b1e4bd438295b0accee10e439a2a
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 11:22:33 +0700

    Pass adding getPasswordByUsername method

--- auth-api/src/Domains/users/_test/UserRepository.test.js
+++ auth-api/src/Domains/users/_test/UserRepository.test.js
@@ -8,5 +8,6 @@ describe('UserRepository interface', () => {
     // Action and Assert
     await expect(userRepository.addUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getPasswordByUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
