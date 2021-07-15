7493e8a6e8547a84dbfb3fdabd6f851330d6c1c1
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 15:32:21 +0700

    add needed method for refresh authentication use case

--- auth-api/src/Applications/security/AuthenticationTokenManager.js
+++ auth-api/src/Applications/security/AuthenticationTokenManager.js
@@ -6,6 +6,14 @@ class AuthenticationTokenManager {
   async createAccessToken(payload) {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
+
+  async verifyRefreshToken(token) {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async decodePayload() {
+    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = AuthenticationTokenManager;
