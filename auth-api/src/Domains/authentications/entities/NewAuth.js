ba17cc2f32028e106de25a9a9def9063f7b41b95
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 11:32:06 +0700

    Pass NewAuth entities test

---
+++ auth-api/src/Domains/authentications/entities/NewAuth.js
@@ -0,0 +1,22 @@
+class NewAuth {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    this.accessToken = payload.accessToken;
+    this.refreshToken = payload.refreshToken;
+  }
+
+  _verifyPayload(payload) {
+    const { accessToken, refreshToken } = payload;
+
+    if (!accessToken || !refreshToken) {
+      throw new Error('NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof accessToken !== 'string' || typeof refreshToken !== 'string') {
+      throw new Error('NEW_AUTH.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = NewAuth;
---
+++ auth-api/src/Domains/authentications/entities/_test/NewAuth.test.js
@@ -0,0 +1,40 @@
+const NewAuth = require('../NewAuth');
+
+describe('NewAuth entities', () => {
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      accessToken: 'accessToken',
+    };
+
+    // Action & Assert
+    expect(() => new NewAuth(payload)).toThrowError('NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      accessToken: 'accessToken',
+      refreshToken: 1234,
+    };
+
+    // Action & Assert
+    expect(() => new NewAuth(payload)).toThrowError('NEW_AUTH.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create NewAuth entities correctly', () => {
+    // Arrange
+    const payload = {
+      accessToken: 'accessToken',
+      refreshToken: 'refreshToken',
+    };
+
+    // Action
+    const newAuth = new NewAuth(payload);
+
+    // Assert
+    expect(newAuth).toBeInstanceOf(NewAuth);
+    expect(newAuth.accessToken).toEqual(payload.accessToken);
+    expect(newAuth.refreshToken).toEqual(payload.refreshToken);
+  });
+});
