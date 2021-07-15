bf7deb4d470054484ef754ef144d082c4d0eea00
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 14:15:25 +0700

    Implement comparePassword

--- auth-api/src/Infrastructures/security/BcryptEncryptionHelper.js
+++ auth-api/src/Infrastructures/security/BcryptEncryptionHelper.js
@@ -1,4 +1,5 @@
 const EncryptionHelper = require('../../Applications/security/EncryptionHelper');
+const AuthenticationError = require('../../Commons/exceptions/AuthenticationError');
 
 class BcryptEncryptionHelper extends EncryptionHelper {
   constructor(bcrypt, saltRound = 10) {
@@ -9,6 +10,14 @@ class BcryptEncryptionHelper extends EncryptionHelper {
 
   async encryptPassword(password) {
     return this._bcrypt.hash(password, this._saltRound);
+  }
+
+  async comparePassword(password, hashedPassword) {
+    const result = await this._bcrypt.compare(password, hashedPassword);
+
+    if (!result) {
+      throw new AuthenticationError('kredensial yang Anda masukkan salah');
+    }
   }
 }
 
--- auth-api/src/Infrastructures/security/_test/BcryptEncryptionHelper.test.js
+++ auth-api/src/Infrastructures/security/_test/BcryptEncryptionHelper.test.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const EncryptionHelper = require('../../../Applications/security/EncryptionHelper');
+const AuthenticationError = require('../../../Commons/exceptions/AuthenticationError');
 const BcryptEncryptionHelper = require('../BcryptEncryptionHelper');
 
 describe('BcryptEncryptionHelper', () => {
@@ -22,6 +23,29 @@ describe('BcryptEncryptionHelper', () => {
       expect(typeof encryptedPassword).toEqual('string');
       expect(encryptedPassword).not.toEqual('plain_password');
       expect(spyHash).toBeCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptEncryptionHelper
+    });
+  });
+
+  describe('comparePassword function', () => {
+    it('should throw AuthenticationError if password not match', async () => {
+      // Arrange
+      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+
+      // Act & Assert
+      await expect(bcryptEncryptionHelper.comparePassword('plain_password', 'encrypted_password'))
+        .rejects
+        .toThrow(AuthenticationError);
+    });
+
+    it('should not return AuthenticationError if password match', async () => {
+      // Arrange
+      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+      const plainPassword = 'secret';
+      const encryptedPassword = await bcryptEncryptionHelper.encryptPassword(plainPassword);
+
+      // Act & Assert
+      await expect(bcryptEncryptionHelper.comparePassword(plainPassword, encryptedPassword))
+        .resolves.not.toThrow(AuthenticationError);
     });
   });
 });
