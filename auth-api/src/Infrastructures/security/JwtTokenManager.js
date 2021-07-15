f27ab3596ea66b4afa0edcb26e1917900d644ae3
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 23:13:04 +0700

    Adding verifyRefreshToken function

--- auth-api/src/Infrastructures/security/JwtTokenManager.js
+++ auth-api/src/Infrastructures/security/JwtTokenManager.js
@@ -1,4 +1,5 @@
 const AuthenticationTokenManager = require('../../Applications/security/AuthenticationTokenManager');
+const InvariantError = require('../../Commons/exceptions/InvariantError');
 
 class JwtTokenManager extends AuthenticationTokenManager {
   constructor(jwt) {
@@ -12,6 +13,15 @@ class JwtTokenManager extends AuthenticationTokenManager {
 
   async createRefreshToken(payload) {
     return this._jwt.generate(payload, process.env.REFRESH_TOKEN_KEY);
+  }
+
+  async verifyRefreshToken(token) {
+    try {
+      const artifacts = this._jwt.decode(token);
+      this._jwt.verify(artifacts, process.env.REFRESH_TOKEN_KEY);
+    } catch (error) {
+      throw new InvariantError('refresh token tidak valid');
+    }
   }
 }
 
