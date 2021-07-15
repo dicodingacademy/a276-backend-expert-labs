4602ebee4c38b87c7b77473032cbb60146d2cd14
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 22:43:10 +0700

    adding some message

--- auth-api/src/Commons/exceptions/DomainErrorTranslator.js
+++ auth-api/src/Commons/exceptions/DomainErrorTranslator.js
@@ -13,6 +13,8 @@ DomainErrorTranslator._directories = {
   'NEW_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
   'USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
   'USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
+  'REFRESH_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN': new InvariantError('harus mengirimkan token refresh'),
+  'REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
 };
 
 module.exports = DomainErrorTranslator;
