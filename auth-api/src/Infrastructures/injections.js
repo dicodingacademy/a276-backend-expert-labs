bee1229b59132a0ff1395d9d356c73257f680e97
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 14:57:00 +0700

    add login use case and service to injections

--- auth-api/src/Infrastructures/injections.js
+++ auth-api/src/Infrastructures/injections.js
@@ -3,22 +3,34 @@
 // external agency
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
+const Jwt = require('@hapi/jwt');
 const pool = require('./database/postgres/pool');
 
 // service (repository, helper, manager, etc)
 const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
+const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
 const BcryptEncryptionHelper = require('./security/BcryptEncryptionHelper');
+const JwtTokenManager = require('./security/JwtTokenManager');
 
 // use case
 const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
+const LoginUserUseCase = require('../Applications/use_case/LoginUserUseCase');
 
 const serviceInstanceContainer = {
   userRepository: new UserRepositoryPostgres(pool, nanoid),
+  authenticationRepository: new AuthenticationRepositoryPostgres(pool),
   encryptionHelper: new BcryptEncryptionHelper(bcrypt),
+  jwtTokenManager: new JwtTokenManager(Jwt.token),
 };
 
 const useCaseInstaceContainer = {
   addUserUseCase: new AddUserUseCase({
+    userRepository: serviceInstanceContainer.userRepository,
+    encryptionHelper: serviceInstanceContainer.encryptionHelper,
+  }),
+  loginUserUseCase: new LoginUserUseCase({
+    authenticationRepository: serviceInstanceContainer.authenticationRepository,
+    authenticationTokenManager: serviceInstanceContainer.jwtTokenManager,
     userRepository: serviceInstanceContainer.userRepository,
     encryptionHelper: serviceInstanceContainer.encryptionHelper,
   }),
