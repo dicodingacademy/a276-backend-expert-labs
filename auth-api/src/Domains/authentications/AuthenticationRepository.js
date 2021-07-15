6c0dee7f284475c986c46ac56f597d27e84dcd29
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 11:36:16 +0700

    pass AuthenticationRepository interface unit test

--- auth-api/src/Domains/authentications/AuthenticationRepository.js
+++ auth-api/src/Domains/authentications/AuthenticationRepository.js
@@ -1,13 +1,5 @@
 class AuthenticationRepository {
-  async addToken(token) {
-    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
-
-  async checkAvailabilityToken(token) {
-    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
-
-  async deleteToken(token) {
+  addToken(token) {
     throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
--- auth-api/src/Domains/authentications/_test/AuthenticationRepository.test.js
+++ auth-api/src/Domains/authentications/_test/AuthenticationRepository.test.js
@@ -1,13 +1,11 @@
 const AuthenticationRepository = require('../AuthenticationRepository');
 
 describe('AuthenticationRepository interface', () => {
-  it('should throw error when invoke unimplemented method', async () => {
+  it('should throw error when invoke unimplemented method', () => {
     // Arrange
     const authenticationRepository = new AuthenticationRepository();
 
     // Action & Assert
-    await expect(authenticationRepository.addToken('')).rejects.toThrowError('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(authenticationRepository.checkAvailabilityToken('')).rejects.toThrowError('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(authenticationRepository.deleteToken('')).rejects.toThrowError('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    expect(() => authenticationRepository.addToken('')).toThrowError('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
