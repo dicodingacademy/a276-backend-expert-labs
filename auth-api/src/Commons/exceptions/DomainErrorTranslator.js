5e8903f55f012ece9c973c4daefed2f22b3a132f
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 15:25:42 +0700

    translate some message

--- auth-api/src/Commons/exceptions/DomainErrorTranslator.js
+++ auth-api/src/Commons/exceptions/DomainErrorTranslator.js
@@ -11,6 +11,8 @@ DomainErrorTranslator._directories = {
   'NEW_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
   'NEW_USER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
   'NEW_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang'),
+  'USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan username dan password'),
+  'USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('username dan password harus string'),
 };
 
 module.exports = DomainErrorTranslator;
