c86cc1eb431c37e32566dd548510d57ef0a6b917
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 15:25:52 +0700

    pass server test

--- auth-api/src/Interfaces/http/api/authentications/handler.js
+++ auth-api/src/Interfaces/http/api/authentications/handler.js
@@ -1,16 +1,8 @@
 class AuthenticationsHandler {
-  constructor({
-    loginUserUseCase,
-    refreshAuthenticationUseCase,
-    logoutUserUseCase,
-  }) {
+  constructor({ loginUserUseCase }) {
     this._loginUserUseCase = loginUserUseCase;
-    this._refreshAuthenticationUseCase = refreshAuthenticationUseCase;
-    this._logoutUserUseCase = logoutUserUseCase;
 
     this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this);
-    this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
-    this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
   }
 
   async postAuthenticationHandler(request, h) {
@@ -24,25 +16,6 @@ class AuthenticationsHandler {
     });
     response.code(201);
     return response;
-  }
-
-  async putAuthenticationHandler(request, h) {
-    const accessToken = await this._refreshAuthenticationUseCase
-      .execute(request.payload);
-
-    return {
-      status: 'success',
-      data: {
-        accessToken,
-      },
-    };
-  }
-
-  async deleteAuthenticationHandler(request) {
-    await this._logoutUserUseCase.execute(request.payload);
-    return {
-      status: 'success',
-    };
   }
 }
 
--- auth-api/src/Interfaces/http/api/authentications/routes.js
+++ auth-api/src/Interfaces/http/api/authentications/routes.js
@@ -4,16 +4,6 @@ const routes = (handler) => ([
     path: '/authentications',
     handler: handler.postAuthenticationHandler,
   },
-  {
-    method: 'PUT',
-    path: '/authentications',
-    handler: handler.putAuthenticationHandler,
-  },
-  {
-    method: 'DELETE',
-    path: '/authentications',
-    handler: handler.deleteAuthenticationHandler,
-  },
 ]);
 
 module.exports = routes;
