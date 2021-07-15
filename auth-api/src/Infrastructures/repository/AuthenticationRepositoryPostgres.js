7fcc75d115232e38ebb14a25f77368e3641927a7
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 22:51:10 +0700

    Adding checkAvailabilityToken method

--- auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
+++ auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -1,3 +1,4 @@
+const InvariantError = require('../../Commons/exceptions/InvariantError');
 const AuthenticationRepository = require('../../Domains/authentications/AuthenticationRepository');
 
 class AuthenticationRepositoryPostgres extends AuthenticationRepository {
@@ -13,6 +14,19 @@ class AuthenticationRepositoryPostgres extends AuthenticationRepository {
     };
 
     await this._pool.query(query);
+  }
+
+  async checkAvailabilityToken(token) {
+    const query = {
+      text: 'SELECT * FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length === 0) {
+      throw new InvariantError('refresh token tidak ditemukan di database');
+    }
   }
 }
 
