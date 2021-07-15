d07651e97c4d211c57d1031b53b458364cbf6540
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 15:28:26 +0700

    add checkAvailabilityToken method

--- auth-api/src/Domains/authentications/AuthenticationRepository.js
+++ auth-api/src/Domains/authentications/AuthenticationRepository.js
@@ -2,6 +2,10 @@ class AuthenticationRepository {
   async addToken(token) {
     throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async checkAvailabilityToken(token) {
+    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = AuthenticationRepository;
