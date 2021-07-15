08fc8d960195bc65fe661752d8c865bf353dc731
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-16 00:24:01 +0700

    add delete token method

--- auth-api/src/Domains/authentications/AuthenticationRepository.js
+++ auth-api/src/Domains/authentications/AuthenticationRepository.js
@@ -6,6 +6,10 @@ class AuthenticationRepository {
   async checkAvailabilityToken(token) {
     throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteToken(token) {
+    throw new Error('AUTHENTICATION_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = AuthenticationRepository;
