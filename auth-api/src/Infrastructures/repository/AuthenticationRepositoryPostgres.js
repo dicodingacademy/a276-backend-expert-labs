39eb1440170964836191e674f88acfda33503543
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 12:54:22 +0700

    Pass creating AuthenticationRepositoryPostgres

--- auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
+++ auth-api/src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -1,4 +1,3 @@
-const InvariantError = require('../../Commons/exceptions/InvariantError');
 const AuthenticationRepository = require('../../Domains/authentications/AuthenticationRepository');
 
 class AuthenticationRepositoryPostgres extends AuthenticationRepository {
@@ -10,28 +9,6 @@ class AuthenticationRepositoryPostgres extends AuthenticationRepository {
   async addToken(token) {
     const query = {
       text: 'INSERT INTO authentications VALUES ($1)',
-      values: [token],
-    };
-
-    await this._pool.query(query);
-  }
-
-  async checkAvailabilityToken(token) {
-    const query = {
-      text: 'SELECT * FROM authentications WHERE token = $1',
-      values: [token],
-    };
-
-    const result = await this._pool.query(query);
-
-    if (result.rows.length === 0) {
-      throw new InvariantError('refresh token tidak ditemukan di database');
-    }
-  }
-
-  async deleteToken(token) {
-    const query = {
-      text: 'DELETE FROM authentications WHERE token = $1',
       values: [token],
     };
 
--- auth-api/src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
+++ auth-api/src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
@@ -1,4 +1,3 @@
-const InvariantError = require('../../../Commons/exceptions/InvariantError');
 const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
 const AuthenticationRepository = require('../../../Domains/authentications/AuthenticationRepository');
 const pool = require('../../database/postgres/pool');
@@ -32,45 +31,6 @@ describe('AuthenticationRepository postgres', () => {
         const tokens = await AuthenticationsTableTestHelper.findToken(token);
         expect(tokens).toHaveLength(1);
         expect(tokens[0].token).toBe(token);
-      });
-    });
-
-    describe('checkAvailabilityToken function', () => {
-      it('should throw InvariantError if token not available', async () => {
-        // Arrange
-        const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
-        const token = 'token';
-
-        // Action & Assert
-        await expect(authenticationRepository.checkAvailabilityToken(token))
-          .rejects.toThrow(InvariantError);
-      });
-
-      it('should not throw InvariantError if token available', async () => {
-        // Arrange
-        const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
-        const token = 'token';
-        await AuthenticationsTableTestHelper.addToken(token);
-
-        // Action & Assert
-        await expect(authenticationRepository.checkAvailabilityToken(token))
-          .resolves.not.toThrow(InvariantError);
-      });
-    });
-
-    describe('deleteToken', () => {
-      it('should delete token from database', async () => {
-        // Arrange
-        const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
-        const token = 'token';
-        await AuthenticationsTableTestHelper.addToken(token);
-
-        // Action
-        await authenticationRepository.deleteToken(token);
-
-        // Assert
-        const tokens = await AuthenticationsTableTestHelper.findToken(token);
-        expect(tokens).toHaveLength(0);
       });
     });
   });
