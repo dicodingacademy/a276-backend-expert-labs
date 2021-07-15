133e4f1ba3d246caf9327ac303b20f7b7cb12a90
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 14:32:02 +0700

    remove unused method

--- auth-api/src/Applications/security/AuthenticationTokenManager.js
+++ auth-api/src/Applications/security/AuthenticationTokenManager.js
@@ -6,10 +6,6 @@ class AuthenticationTokenManager {
   async createAccessToken(payload) {
     throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   }
-
-  async verifyRefreshToken(token) {
-    throw new Error('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-  }
 }
 
 module.exports = AuthenticationTokenManager;
--- auth-api/src/Applications/security/_test/AuthenticationTokenManager.test.js
+++ auth-api/src/Applications/security/_test/AuthenticationTokenManager.test.js
@@ -8,6 +8,5 @@ describe('AuthenticationTokenManager interface', () => {
     // Action & Assert
     await expect(tokenManager.createAccessToken('')).rejects.toThrowError('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
     await expect(tokenManager.createRefreshToken('')).rejects.toThrowError('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
-    await expect(tokenManager.verifyRefreshToken('')).rejects.toThrowError('AUTHENTICATION_TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED');
   });
 });
