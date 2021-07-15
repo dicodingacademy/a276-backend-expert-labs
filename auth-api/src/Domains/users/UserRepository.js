59986307665359b3d9ee3bc3d3b2f95e40aff3bb
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-07-15 11:22:33 +0700

    Pass adding getPasswordByUsername method

--- auth-api/src/Domains/users/UserRepository.js
+++ auth-api/src/Domains/users/UserRepository.js
@@ -6,6 +6,10 @@ class UserRepository {
   async verifyAvailableUsername(username) {
     throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async getPasswordByUsername(username) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = UserRepository;
