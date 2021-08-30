b48f79076fcc59d8b0977a74d5ddc15ab0f5d7fc
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-08-30 11:41:23 +0700

    remove instanceof assert test

--- auth-api/src/Commons/exceptions/_test/AuthenticationError.test.js
+++ auth-api/src/Commons/exceptions/_test/AuthenticationError.test.js
@@ -5,10 +5,6 @@ describe('AuthenticationError', () => {
   it('should create AuthenticationError correctly', () => {
     const authenticationError = new AuthenticationError('authentication error!');
 
-    expect(authenticationError).toBeInstanceOf(AuthenticationError);
-    expect(authenticationError).toBeInstanceOf(ClientError);
-    expect(authenticationError).toBeInstanceOf(Error);
-
     expect(authenticationError.statusCode).toEqual(401);
     expect(authenticationError.message).toEqual('authentication error!');
     expect(authenticationError.name).toEqual('AuthenticationError');
--- auth-api/src/Commons/exceptions/_test/InvariantError.test.js
+++ auth-api/src/Commons/exceptions/_test/InvariantError.test.js
@@ -5,10 +5,6 @@ describe('InvariantError', () => {
   it('should create an error correctly', () => {
     const invariantError = new InvariantError('an error occurs');
 
-    expect(invariantError).toBeInstanceOf(InvariantError);
-    expect(invariantError).toBeInstanceOf(ClientError);
-    expect(invariantError).toBeInstanceOf(Error);
-
     expect(invariantError.statusCode).toEqual(400);
     expect(invariantError.message).toEqual('an error occurs');
     expect(invariantError.name).toEqual('InvariantError');
