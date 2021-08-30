8f1c1c72f25f8065cd07033343199f2b4481ea9a
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-08-30 11:41:23 +0700

    remove instanceof assert test

--- auth-api/src/Commons/exceptions/_test/NotFoundError.test.js
+++ auth-api/src/Commons/exceptions/_test/NotFoundError.test.js
@@ -5,10 +5,6 @@ describe('NotFoundError', () => {
   it('should create error correctly', () => {
     const notFoundError = new NotFoundError('not found!');
 
-    expect(notFoundError).toBeInstanceOf(NotFoundError);
-    expect(notFoundError).toBeInstanceOf(ClientError);
-    expect(notFoundError).toBeInstanceOf(Error);
-
     expect(notFoundError.message).toEqual('not found!');
     expect(notFoundError.statusCode).toEqual(404);
     expect(notFoundError.name).toEqual('NotFoundError');
