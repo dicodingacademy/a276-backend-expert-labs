4bcc04a9850bd98d968073bb2a6a159dc6ce5fb2
Aras <aras.opraza@gmail.com>
2023-12-27 16:52:01 +0700

    create object config for environment server anddatabase

---
+++ auth-api/src/Commons/config.js
@@ -0,0 +1,25 @@
+/* istanbul ignore file */
+const config = {
+  app: {
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    debug: process.env.NODE_ENV === 'development' ? { request: ['error'] } : {},
+  },
+  testConfig: {
+    host: process.env.PGHOST_TEST,
+    port: process.env.PGPORT_TEST,
+    user: process.env.PGUSER_TEST,
+    password: process.env.PGPASSWORD_TEST,
+    database: process.env.PGDATABASE_TEST,
+  },
+  devConfig: {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
+  JWT_STRATEGY: 'forumapi',
+};
+
+module.exports = config;
