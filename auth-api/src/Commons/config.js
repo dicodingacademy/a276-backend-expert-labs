35136c3e90836191c976683f311fde0a91c1317b
Aras <aras.opraza@gmail.com>
2024-01-03 14:18:18 +0700

    change implementation of config for test

--- auth-api/src/Commons/config.js
+++ auth-api/src/Commons/config.js
@@ -1,24 +1,33 @@
 /* istanbul ignore file */
+const dotenv = require('dotenv')
+const path = require('path')
+
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({
+    path: path.resolve(process.cwd(), '.test.env')
+  })
+} else {
+  dotenv.config()
+}
+
 const config = {
   app: {
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     port: process.env.PORT,
     debug: process.env.NODE_ENV === 'development' ? { request: ['error'] } : {},
   },
-  testConfig: {
-    host: process.env.PGHOST_TEST,
-    port: process.env.PGPORT_TEST,
-    user: process.env.PGUSER_TEST,
-    password: process.env.PGPASSWORD_TEST,
-    database: process.env.PGDATABASE_TEST,
-  },
-  devConfig: {
+  database: {
     host: process.env.PGHOST,
     port: process.env.PGPORT,
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
   },
+  auth:{
+    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+    refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
+    accessTokenAge: process.env.ACCESS_TOKEN_AGE
+  }
 };
 
 module.exports = config;
