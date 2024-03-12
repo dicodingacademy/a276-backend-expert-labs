f86751e49d3baf75bf4364065a9ed016ebb08ef9
arasopraza <aras.opraza@gmail.com>
2024-03-12 22:53:29 +0700

    fix: error pool.js config database

--- auth-api/src/Infrastructures/database/postgres/pool.js
+++ auth-api/src/Infrastructures/database/postgres/pool.js
@@ -2,6 +2,6 @@
 const { Pool } = require('pg');
 const config = require('../../../Commons/config');
 
-const pool = new Pool(config.testConfig);
+const pool = new Pool(config.database);
 
 module.exports = pool;
