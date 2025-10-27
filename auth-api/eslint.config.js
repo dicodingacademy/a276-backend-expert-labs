e67c9c3a8f5c4093003d44e34257b474d3f1e4f7
arasopraza <aras.opraza@gmail.com>
2025-10-27 10:04:30 +0700

    chore: add globals package and update ESLint configuration for node globals

--- auth-api/eslint.config.js
+++ auth-api/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import daStyle from 'eslint-config-dicodingacademy';
 import vitest from '@vitest/eslint-plugin';
+import globals from 'globals';
 
 export default defineConfig([
   {
@@ -10,5 +11,5 @@ export default defineConfig([
     },
   },
   daStyle,
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: { ...vitest.environments.env.globals, } } },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: { ...vitest.environments.env.globals, ...globals.node } } },
 ]);
